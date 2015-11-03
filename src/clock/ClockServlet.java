bde91f63f0731b5f6c6af566dfa786741cb0d59f
Harout360 <gamemasterhg@sbcglobal.net>
2015-11-02 23:17:12 -0800

    Datastore
    Stores user email and timezone offset to Datastore and retrieves it on
    login.

--- src/clock/ClockServlet.java
+++ src/clock/ClockServlet.java
@@ -11,6 +11,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.EntityNotFoundException;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
 import com.google.appengine.api.users.User;
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
@@ -29,6 +35,25 @@ public class ClockServlet extends HttpServlet {
 		req.setAttribute("user", user);
 		req.setAttribute("loginUrl", loginUrl);
 		req.setAttribute("logoutUrl", logoutUrl);
+		
+		Entity userPrefs = null;
+		if(user != null){
+			DatastoreService ds = DatastoreServiceFactory.getDatastoreService();
+			Key userKey = KeyFactory.createKey("UserPrefs", user.getUserId());
+			try{
+				userPrefs = ds.get(userKey);
+			} catch (EntityNotFoundException e){
+				//no user prefs stored
+			}
+		}
+		if (userPrefs != null) {
+			double tzOffset = ((Double) userPrefs.getProperty("tz_offset")).doubleValue();
+			fmt.setTimeZone(new SimpleTimeZone((int) (tzOffset * 60 * 60 * 1000), ""));
+			req.setAttribute("tzOffset", tzOffset);
+		} else {
+			req.setAttribute("tzOffset", 0);
+		}
+		
 		req.setAttribute("currentTime", fmt.format(new Date()));
 		
 		resp.setContentType("text/html");
